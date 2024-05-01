da0da56b02eb7baf34ce4d0128ced78fae3295d2
Gerard Hernandez <ger.almenara@gmail.com>
2024-05-01 15:29:09 -0700

    Add @types/eslint dependency and fix stroke-linecap and stroke-linejoin attributes in heading.tsx

--- src/components/heading.tsx
+++ src/components/heading.tsx
@@ -18,8 +18,8 @@ function Navigation() {
           <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></g>
           <g id="SVGRepo_iconCarrier">
             <g>
